4f258167d22a98a671a565649b9b239d4832b9a4
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2016-01-02 11:29:53 -0800

    Add wheels and fix collisions

--- src/org/team3128/unittest/SwerveDrive2Test.java
+++ src/org/team3128/unittest/SwerveDrive2Test.java
@@ -1,12 +1,15 @@
 package org.team3128.unittest;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.RenderingHints;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -80,7 +83,7 @@ public class SwerveDrive2Test
 		private static final double TURNING_MOTOR_SPEED_DPS = 180; //degrees per second
 		
 		//should be executed after the swerve drive updates
-		public void update(double timeBetweenUpdates)
+		public void update(int millisecondsBetweenUpdates)
 		{
 			driveWheelSpeed = (RobotMath.getCIMExpectedRPM(driveMotor.get()) / 10) * WHEEL_CIRCUMFERENCE;
 			
@@ -88,7 +91,7 @@ public class SwerveDrive2Test
 			
 			double turningAngularSpeed = TURNING_MOTOR_SPEED_DPS  * turnMotor.get();
 			
-			double distanceTurned = turningAngularSpeed * timeBetweenUpdates;
+			double distanceTurned = turningAngularSpeed * (((double)millisecondsBetweenUpdates) / 1000);
 			
 			simulatedHeadingDegrees += distanceTurned;
 			
@@ -106,9 +109,10 @@ public class SwerveDrive2Test
 	}
 	ArrayList<FakeModule> modules = new ArrayList<FakeModule>();
 	
-	final static double kP = .001, kI = 0, kD = 0;
+	final static double kP = .1, kI = 0, kD = 0;
 	final static double ROBOT_MASS_KG = 100;
-	final static double DRIVE_MOTOR_FORCE = 400; //N
+	final static double DRIVE_MOTOR_FORCE = 200; //N
+	final static double COEFFICIENT_OF_FRICTION = 50; // N/(m/s)
 	
 	SwerveDrive2 drive = new SwerveDrive2();
 	
@@ -120,19 +124,21 @@ public class SwerveDrive2Test
 	
 	BufferedImage robotBaseIcon;
 	
+	double wheelbaseRadiusM;
+	
 	boolean windowOpen;
 	
 	double robotSpeedX = 0, robotSpeedY = 0; // in m/s
 	
-	double robotPositionX = WINDOW_WIDTH_M / 2, robotPositionY = WINDOW_HEIGHT_M / 2; //in m from top left
+	double robotPositionX = WINDOW_WIDTH_M / 2.0, robotPositionY = WINDOW_HEIGHT_M / 2.0; //in m from top left
 	
 	final static int PIXELS_PER_METER = 300;
 
 	final static int WINDOW_WIDTH_PX = 1920;
 	final static int WINDOW_HEIGHT_PX = 1080;
 
-	final static double WINDOW_WIDTH_M = WINDOW_WIDTH_PX / PIXELS_PER_METER;
-	final static double WINDOW_HEIGHT_M = WINDOW_HEIGHT_PX / PIXELS_PER_METER;
+	final static double WINDOW_WIDTH_M = ((double)WINDOW_WIDTH_PX) / PIXELS_PER_METER;
+	final static double WINDOW_HEIGHT_M = ((double)WINDOW_HEIGHT_PX) / PIXELS_PER_METER;
 	
 
 	@Test
@@ -141,7 +147,8 @@ public class SwerveDrive2Test
 		
         try
 		{
-			robotBaseIcon = ImageIO.read(getClass().getResource("assets/robot-base.png"));
+			robotBaseIcon = ImageIO.read(getClass().getResource("assets/robot-base.png")); //must be square
+			wheelbaseRadiusM = (robotBaseIcon.getWidth() / 2.0) / PIXELS_PER_METER; 
 		}
 		catch(IOException e)
 		{
@@ -202,7 +209,7 @@ public class SwerveDrive2Test
 		listenerManager.tick();
 		
 		//update swerve drive class
-		drive.drive(listenerManager.getRawAxis(ControllerSimXbox.JOY1X), listenerManager.getRawAxis(ControllerSimXbox.JOY1Y), listenerManager.getRawAxis(ControllerSimXbox.JOY2X));		
+		drive.drive(listenerManager.getRawAxis(ControllerSimXbox.JOY1X), -listenerManager.getRawAxis(ControllerSimXbox.JOY1Y), listenerManager.getRawAxis(ControllerSimXbox.JOY2X));		
 		
 		//update fake swerve modules
 		for(FakeModule module : modules)
@@ -222,6 +229,10 @@ public class SwerveDrive2Test
 			netForceY += Math.sin(driveWheelAngleRad) * module.driveWheelForce;
 		}
 		
+		netForceX += -1 * COEFFICIENT_OF_FRICTION * RobotMath.square(robotSpeedX) * RobotMath.sgn(robotSpeedX);
+		netForceY += -1 * COEFFICIENT_OF_FRICTION * RobotMath.square(robotSpeedY) * RobotMath.sgn(robotSpeedY);
+
+		
 		double robotAccelerationX = netForceX / ROBOT_MASS_KG;
 		double robotAccelerationY = netForceY / ROBOT_MASS_KG;
 		
@@ -232,15 +243,39 @@ public class SwerveDrive2Test
 		
 		Log.debug("SwerveDrive2Test", "X Acceleration: " + robotAccelerationX + ", Y Acceleration: " + robotAccelerationY);
 		
-		robotPositionX += robotSpeedX * secondsSinceLastTick;
-		robotPositionY += robotSpeedY * secondsSinceLastTick;
+		double newPositionX = robotPositionX + robotSpeedX * secondsSinceLastTick;
+		double newPositionY = robotPositionY + robotSpeedY * secondsSinceLastTick;
+		
+		//detect collision with wall
+		if((newPositionX - wheelbaseRadiusM) < 0 || (newPositionX + wheelbaseRadiusM) > WINDOW_WIDTH_M)
+		{
+			robotSpeedX *= -.5;
+		}
+		else
+		{
+			robotPositionX = newPositionX;
+		}
+		
+		if((newPositionY - wheelbaseRadiusM) < 0 || (newPositionY + wheelbaseRadiusM) > WINDOW_HEIGHT_M)
+		{
+			robotSpeedY *= -.5;
+		}
+		else
+		{
+			robotPositionY = newPositionY;
+		}
+		
+		//detect wheel direction to motion direction mismatch
+		double motionDirectionDeg = RobotMath.rTD(Math.tan(robotSpeedY/robotSpeedX));
 	}
 	
 	@Ignore
 	class SwerveSimulatorPanel extends JPanel
 	{	
 		int halfRobotHeight, halfRobotWidth;
-
+		
+		int wheelbaseRadiusPx;
+		
 		/**
 		 * 
 		 */
@@ -250,26 +285,55 @@ public class SwerveDrive2Test
 		{
 			halfRobotHeight = robotBaseIcon.getHeight() / 2;
 			halfRobotWidth = robotBaseIcon.getWidth() / 2;
+			
+			wheelbaseRadiusPx = robotBaseIcon.getWidth() / 2 - 20;
 		}
 		
 		public void paintComponent(Graphics g)
 		{
 			super.paintComponent(g);
 			
+			int robotCenterPxX = (int)(robotPositionX * PIXELS_PER_METER);
+			int robotCenterPxY = (int)(robotPositionY * PIXELS_PER_METER);
+
 			Graphics2D g2d = (Graphics2D) g;
 			
 			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 			
 			//draw robot
-			g2d.drawImage(robotBaseIcon, ((int)(robotPositionX * PIXELS_PER_METER)) - halfRobotWidth, ((int)(robotPositionY * PIXELS_PER_METER)) - halfRobotWidth, null);
+			g2d.drawImage(robotBaseIcon, robotCenterPxX - halfRobotWidth, robotCenterPxY - halfRobotHeight, null);
 			
 			for(int index = 0; index < modules.size(); ++index)
 			{
 				FakeModule currentModule = modules.get(index);
-				g2d.drawString(String.format("Module %d: Drive Speed (RPM): %f, Heading Offset: %f", index, currentModule.driveWheelSpeed, currentModule.simulatedHeadingDegrees), 0, index * 20);
+				g2d.drawString(String.format("Module %d: Drive Speed (RPM): %f, Heading Offset (deg): %f", index, currentModule.driveWheelSpeed, currentModule.simulatedHeadingDegrees), 0, index * 20);
 			}
 			
-		
+			final int wheelWidth = 25;
+			final int wheelHeight = 6;
+			
+			//draw modules
+			for(FakeModule module : modules)
+			{
+				int pixelOffsetX = RobotMath.floor_double_int(wheelbaseRadiusPx * Math.cos(RobotMath.dTR(module.wheelbaseAngle)));
+				int pixelOffsetY = RobotMath.floor_double_int(wheelbaseRadiusPx * Math.sin(RobotMath.dTR(module.wheelbaseAngle)));
+				
+				Rectangle wheelRect = new Rectangle(robotCenterPxX + pixelOffsetX - (wheelWidth/2), robotCenterPxY + pixelOffsetY - (wheelHeight/2), wheelWidth, wheelHeight);
+				
+				//from http://stackoverflow.com/questions/8807717/java-rotate-rectangle-around-the-center
+				AffineTransform transform = new AffineTransform();
+				transform.rotate(RobotMath.dTR(module.simulatedHeadingDegrees + 90), robotCenterPxX + pixelOffsetX, robotCenterPxY + pixelOffsetY);
+				AffineTransform old = g2d.getTransform();
+				g2d.transform(transform);
+
+				g2d.draw(wheelRect);
+				
+				g2d.setColor(Color.WHITE);
+				
+				g2d.fill(wheelRect);
+
+				g2d.setTransform(old);
+			}
 		}
 	}
 	
